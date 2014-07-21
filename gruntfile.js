34a4183aa9a4274dac513a6b2878db3f2c247f07
Etienne Brodu <etnbrdcom@gmail.com>
2014-07-21 15:55:39 +0200

    test travis gh-pages publish

--- gruntfile.js
+++ gruntfile.js
@@ -34,12 +34,59 @@ module.exports = function(grunt) {
           title: 'flx'
         }
       }
+    },
+
+    'gh-pages': {
+      options: {
+        branch: 'gh-pages',
+        base: 'docs'
+      },
+      deploy: {
+        options: {
+          user: {
+            name: 'gh-pages',
+            email: 'etn@etnbrd.com'
+          },
+          repo: 'https://' + process.env.GH_TOKEN + '@github.com/etnbrd/flx-lib.git',
+          message: 'publish gh-pages (auto)' + getDeployMessage(),
+          silent: true
+        },
+        src: ['**/*']
+      }
     }
+
   });
+
+  function getDeployMessage() {
+    var ret = '\n\n';
+    if (process.env.TRAVIS !== 'true') {
+      ret += 'missing env vars for travis-ci';
+      return ret;
+    }
+    ret += 'branch: ' + process.env.TRAVIS_BRANCH + '\n';
+    ret += 'SHA: ' + process.env.TRAVIS_COMMIT + '\n';
+    ret += 'range SHA: ' + process.env.TRAVIS_COMMIT_RANGE + '\n';
+    ret += 'build id: ' + process.env.TRAVIS_BUILD_ID + '\n';
+    ret += 'build number: ' + process.env.TRAVIS_BUILD_NUMBER + '\n';
+    return ret;
+  }
+
+  function deploy(){
+    if (process.env.TRAVIS === 'true' && process.env.TRAVIS_SECURE_ENV_VARS === 'true' && process.env.TRAVIS_PULL_REQUEST === 'false') {
+      grunt.log.writeln('executing deployment');
+      grunt.task.run('gh-pages:publish');
+    } else {
+      grunt.log.writeln('skipped deployment');
+    }
+  }
 
   grunt.loadNpmTasks('grunt-docco');
   grunt.loadNpmTasks('grunt-doxx');
   grunt.loadNpmTasks('grunt-simple-mocha');
+  grunt.loadNpmTasks('grunt-gh-pages');
 
   grunt.registerTask('default', ['simplemocha', 'docco']);
+
+  grunt.registerTask('deploy', deploy);
+  grunt.registerTask('publish', ['docco', 'deploy']);
 };
--- readme.md
+++ readme.md
@@ -1,5 +1,7 @@
 # Flx : a fluxionnal execution model
 
+[Commented source code](http://etnbrd.github.io/flx-lib/).
+
 [![NPM version](https://badge.fury.io/js/flx.svg)](http://badge.fury.io/js/flx)
 [![Build Status](https://travis-ci.org/etnbrd/flx-lib.svg?branch=master)](https://travis-ci.org/etnbrd/flx-lib)
 [![Code Climate](https://codeclimate.com/github/etnbrd/flx-lib.png)](https://codeclimate.com/github/etnbrd/flx-lib)
